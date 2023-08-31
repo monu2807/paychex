7cd48cc8f251c3f9b264b7d65646f551a850d8a6
monu2807 <monu.sagar@advantal.net>
2023-08-31 17:43:23 +0530

    hello

--- src/main/java/com/swagger/TestApplication.java
+++ src/main/java/com/swagger/TestApplication.java
@@ -9,8 +9,7 @@ public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 		System.out.println("Run Project");
-		System.out.println("done project");
-		System.out.println("success project");
+	   System.out.println("gui testing ");
 	}
 
 }
