5254bfd2694c38b34831536d178e6250bb38d6e2
monu2807 <monu.sagar@advantal.net>
2023-09-04 11:44:43 +0530

    add line

--- src/main/java/com/swagger/TestApplication.java
+++ src/main/java/com/swagger/TestApplication.java
@@ -8,7 +8,7 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
-	System.out.println("test the git);
+	System.out.println("learn the github");
 		
 		
 	}
