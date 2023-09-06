742f38242fb4e1e999f4243340bc62934aa1a179
monu2807 <143709834+monu2807@users.noreply.github.com>
2023-09-06 16:45:20 +0530

    Update TestApplication.java

--- src/main/java/com/swagger/TestApplication.java
+++ src/main/java/com/swagger/TestApplication.java
@@ -8,7 +8,8 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
-    
+
+		System.out.println("run project")
 	}
 
 }
